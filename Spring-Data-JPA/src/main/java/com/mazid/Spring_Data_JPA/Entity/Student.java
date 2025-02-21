8d331bad29aa24c75a3193179845b85686c92345
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-21 17:24:21 +0600

    Configer application.proparty and add a entity Student

---
+++ Spring-Data-JPA/src/main/java/com/mazid/Spring_Data_JPA/Entity/Student.java
@@ -0,0 +1,26 @@
+package com.mazid.Spring_Data_JPA.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+  @Id
+  @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  private Long studentId;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String guardianName;
+  private String guardianEmail;
+  private String guardianPhone;
+}
