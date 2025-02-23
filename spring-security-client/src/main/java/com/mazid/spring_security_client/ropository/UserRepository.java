7172a838b737cb4facad984c6d8496ae57350ca0
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-23 14:51:37 +0600

    Deleted unnecessary files

--- spring-security-client/src/main/java/com/mazid/spring_security_client/ropository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mazid.spring_security_client.ropository;
-
-import com.mazid.spring_security_client.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {};
--- spring-security-client/src/main/java/com/mazid/spring_security_client/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.mazid.spring_security_client.service;
-
-
-import com.mazid.spring_security_client.entity.User;
-import com.mazid.spring_security_client.model.UserModel;
-
-public interface UserService {
-    User registerUser(UserModel userModel);
-}
