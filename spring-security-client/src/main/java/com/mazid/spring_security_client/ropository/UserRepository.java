a6a41a3edf85a507db0c1a39216b1ad090fde3e8
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-23 11:07:44 +0600

    solve JpaRepository Error

--- spring-security-client/src/main/java/com/mazid/spring_security_client/ropository/UserRepository.java
+++ spring-security-client/src/main/java/com/mazid/spring_security_client/ropository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.mazid.spring_security_client.ropository;
 
 import com.mazid.spring_security_client.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends jpaRepository<User , Long >{};
+public interface UserRepository extends JpaRepository<User, Long> {};
