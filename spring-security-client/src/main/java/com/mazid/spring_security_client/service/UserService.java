d64831ab7d1191d3d4bdfbd3de663e096dd76e06
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-23 11:40:28 +0600

    add spring security:

--- spring-security-client/src/main/java/com/mazid/spring_security_client/service/UserService.java
+++ spring-security-client/src/main/java/com/mazid/spring_security_client/service/UserService.java
@@ -1,5 +1,9 @@
 package com.mazid.spring_security_client.service;
 
 
+import com.mazid.spring_security_client.entity.User;
+import com.mazid.spring_security_client.model.UserModel;
+
 public interface UserService {
+    User registerUser(UserModel userModel);
 }
