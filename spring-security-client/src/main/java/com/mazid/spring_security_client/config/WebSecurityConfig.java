3d9668f169a700780fa999f658b994ebd4c1437d
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-24 11:10:27 +0600

    add SecurityFilterChain

---
+++ spring-security-client/src/main/java/com/mazid/spring_security_client/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.mazid.spring_security_client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+      return httpSecurity.build();
+    }
+}
